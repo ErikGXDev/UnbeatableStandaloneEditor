54c3d7d75eb5e9f17967c541cf692dd505d36c05
ErikGXDev <54033728+ErikGXDev@users.noreply.github.com>
2026-03-31 12:38:51 +0200

    Little watermark

--- osu.Game.Rulesets.UMania/Edit/Setup/UbBeatmapEncoder.cs
+++ osu.Game.Rulesets.UMania/Edit/Setup/UbBeatmapEncoder.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using osu.Framework.Logging;
@@ -9,79 +10,35 @@ using osu.Game.Audio;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Beatmaps.Formats;
+using osu.Game.Beatmaps.Legacy;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Skinning;
 
 namespace osu.Game.Rulesets.UMania.Edit.Setup
 {
-    public partial class UbBeatmapEncoder : LegacyBeatmapEncoder
+    public partial class UbBeatmapEncoder : UbLegacyBeatmapEncoder
     {
         public UbBeatmapEncoder(IBeatmap beatmap, ISkin? skin)
             : base(beatmap, skin)
         {
 
-            foreach (HitObject hitObject in beatmap.HitObjects.ToList())
-            {
-                var normalSample = hitObject.Samples.FirstOrDefault(s => s.Name == HitSampleInfo.HIT_NORMAL);
-
-                if (normalSample != null)
-                {
-                    string newBank = "zero";
-
-                    switch (normalSample.Bank.ToLowerInvariant())
-                    {
-                        case HitSampleInfo.BANK_NORMAL:
-                            newBank = "zero";
-                            break;
-
-                        case HitSampleInfo.BANK_SOFT:
-                            newBank = HitSampleInfo.BANK_NORMAL;
-                            break;
-
-                        case HitSampleInfo.BANK_DRUM:
-                            newBank = HitSampleInfo.BANK_DRUM;
-                            break;
-
-                        case HitSampleInfo.BANK_STRONG:
-                            newBank = HitSampleInfo.BANK_DRUM;
-                            break;
-
-                        default:
-                            newBank = "zero";
-                            break;
-                    }
-
-                    int index = hitObject.Samples.IndexOf(normalSample);
-                    hitObject.Samples[index] = new HitSampleInfo(
-                        normalSample.Name,
-                        newBank,
-                        normalSample.Suffix,
-                        normalSample.Volume,
-                        false
-                    );
-
-                }
-
-                hitObject.StartTime = (int)hitObject.StartTime;
-
-                if (hitObject is IHasDuration hasDuration && hitObject is not IHasPath)
-                    hasDuration.Duration = Math.Floor(hasDuration.EndTime) - Math.Floor(hitObject.StartTime);
-
-            }
-
-            foreach (TimingControlPoint tcp in beatmap.ControlPointInfo.TimingPoints.ToList())
-            {
-                tcp.Time = Math.Floor(tcp.Time);
-            }
+               
         }
 
+
+        
 
         public void EncodeB(TextWriter writer)
         {
             var tempWriter = new StringWriter();
             tempWriter.NewLine = writer.NewLine;
-
+            
+            writer.WriteLine("// Created with Erik's Standalone Editor");
+            writer.WriteLine("// https://github.com/ErikGXDev/UnbeatableStandaloneEditor");
+            writer.WriteLine("");
+            
+           
             Encode(tempWriter);
 
             string output = tempWriter.ToString();
