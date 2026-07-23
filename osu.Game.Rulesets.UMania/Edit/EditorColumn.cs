b7c1127db2c8ae7c47baad8d7b248d4f5e891ad0
Erik <54033728+ErikGXDev@users.noreply.github.com>
2026-07-23 15:03:03 +0200

    Add number label to columns

--- osu.Game.Rulesets.UMania/Edit/EditorColumn.cs
+++ osu.Game.Rulesets.UMania/Edit/EditorColumn.cs
@@ -21,8 +21,9 @@ namespace osu.Game.Rulesets.UMania.Edit
 
         [Resolved] private UnbeatableHitObjectComposer composer { get; set; } = null!;
 
-        private SpriteText labelText;
-        
+        private SpriteText labelText = null!;
+        private SpriteText keyLabel = null!;
+
         protected override void LoadComplete()
         {
             base.LoadComplete();
@@ -58,6 +59,47 @@ namespace osu.Game.Rulesets.UMania.Edit
             {
                 labelText.Alpha = 0;
             }
+            
+            TopLevelContainer.Add(keyLabel = new SpriteText
+            {
+                Anchor = Anchor.BottomCentre,
+                Origin = Anchor.BottomCentre,
+                Y = -50,
+                Font = OsuFont.GetFont(size: 20, weight: FontWeight.Bold),
+                Colour = Colour4.White,
+                Shadow = true,
+                ShadowColour = Colour4.Black,
+                Alpha = 0,
+            });
+
+            updateKeyLabel();
+            composer.KeyBasedCharting.BindValueChanged(_ => updateKeyLabel());
+        }
+
+        private void updateKeyLabel()
+        {
+            if (!composer.KeyBasedCharting.Value)
+            {
+                keyLabel.Alpha = 0;
+                return;
+            }
+
+            // Reverse the column mapping to find which key (1-6) targets this column.
+            int[] mapping = composer.Is4Key
+                ? new[] { 0, 2, 3, 1, 4, 5 }
+                : new[] { 0, 1, 2, 3, 4, 5 };
+
+            for (int keyIndex = 0; keyIndex < mapping.Length; keyIndex++)
+            {
+                if (mapping[keyIndex] == Index)
+                {
+                    keyLabel.Text = (keyIndex + 1).ToString();
+                    keyLabel.Alpha = 1;
+                    return;
+                }
+            }
+
+            keyLabel.Alpha = 0;
         }
 
         protected override void OnNewDrawableHitObject(DrawableHitObject drawableHitObject)
