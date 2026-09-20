925aa880b690027b54f61676f1d3c42f9485da2f
Erik <54033728+ErikGXDev@users.noreply.github.com>
2026-09-20 17:45:30 +0200

    Fix offset adjustment control not respecting adjust setting (from osu)

--- osu.Game/Screens/Edit/Timing/TapTimingControl.cs
+++ osu.Game/Screens/Edit/Timing/TapTimingControl.cs
@@ -213,7 +213,7 @@ namespace osu.Game.Screens.Edit.Timing
 
             foreach (var cp in currentGroupItems)
             {
-                if (cp is TimingControlPoint tp)
+                if (cp is TimingControlPoint tp && configManager.Get<bool>(OsuSetting.EditorAdjustExistingObjectsOnTimingChanges))
                 {
                     TimingSectionAdjustments.AdjustHitObjectOffset(beatmap, tp, adjust);
                     beatmap.UpdateAllHitObjects();
